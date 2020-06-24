0f1f211f1a511a7ce8eb14e935ff28fa5c4fb51f
Tripti <triptibhangade@gmail.com>
2020-06-24 20:24:52 +0530

    change some configuration in database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_27_095643) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
